af25e5bb902a003b6f4dfcd68e61961d270b6a9a
Valerie Kucher <passechnik214@gmail.com>
2022-12-31 00:36:06 +0200

    add array

---
+++ script.js
@@ -0,0 +1,8 @@
+let shoppingList = [
+    { name: "bananas", number: 5, isBought: true, price: 7, sum: 35 },
+    { name: "milk", number: 1, isBought: false, price: 38, sum: 38 },
+    { name: "oranges", number: 3, isBought: true, price: 15, sum: 45 },
+    { name: "tomatos", number: 8, isBought: false, price: 3, sum: 24 },
+    { name: "bread", number: 1, isBought: true, price: 29, sum: 29 },
+]
+
